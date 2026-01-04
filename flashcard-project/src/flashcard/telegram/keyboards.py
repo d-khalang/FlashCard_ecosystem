7c555f23ff0ec66a658c0682c7711071ad914a90
Dani khalang <danikhalang@gmail.com>
2026-01-04 22:26:57 +0100

    2 sample keyboards to practise with

--- flashcard-project/src/flashcard/telegram/keyboards.py
+++ flashcard-project/src/flashcard/telegram/keyboards.py
@@ -1,8 +1,16 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, 
+                           ReplyKeyboardMarkup, KeyboardButton)
+
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
 def get_explanation_keyboard(card_id: str = "temp_id") -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(text="Save", callback_data=f"save:{card_id}")
     builder.button(text="Regen", callback_data=f"regen:{card_id}")
+    return builder.as_markup()
+
+def get_reply_keyboard():
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Test1")
+    builder.button(text="Test2")
     return builder.as_markup()
