4500847d2c74955d821abe97d482fe2343cbf47e
Dani khalang <danikhalang@gmail.com>
2026-02-19 18:36:51 +0100

    add user data to output

--- flashcard-project/src/flashcard/telegram/helpers/card_generator.py
+++ flashcard-project/src/flashcard/telegram/helpers/card_generator.py
@@ -3,6 +3,7 @@ from flashcard.services.llm.llm import LLMService
 from flashcard.telegram.ui.expression import render_expression_card
 from flashcard.schemas.languages import get_language_flag
 from flashcard.schemas.expression import ExpressionCard
+from flashcard.schemas.user import UserDB
 from flashcard.services.user import UserService
 
 async def generate_and_render_card(
@@ -10,9 +11,9 @@ async def generate_and_render_card(
     user_service: UserService,
     user_id: Union[str, int],
     text: str,
-) -> tuple[str, bool, ExpressionCard]:
+) -> tuple[str, bool, ExpressionCard, UserDB]:
     """
-    Generates an expression card using LLMService and returns the rendered text, success status, and the card object.
+    Generates an expression card using LLMService and returns the rendered text, success status, the card object, and user.
     
     Args:
         llm_service: Instance of LLMService.
@@ -21,7 +22,7 @@ async def generate_and_render_card(
         text: Input text (raw or normalized).
 
     Returns:
-        tuple[str, bool, ExpressionCard]: (rendered_text, success, card_object)
+        tuple[str, bool, ExpressionCard, UserDB]: (rendered_text, success, card_object, user)
     """
 
     user = await user_service.get_user(user_id)
@@ -44,4 +45,5 @@ async def generate_and_render_card(
     success = rendered.get("success", False)
     content = rendered.get("content", "")
     
-    return content, success, card
+    return content, success, card, user
+
